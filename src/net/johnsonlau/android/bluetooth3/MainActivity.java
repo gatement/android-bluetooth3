ab8dfe16b19f0ccd72ad5c8ee93a4e3765ce7c32
Johnson Liu <gatement@gmail.com>
2013-10-27 12:52:24 +0800

    get started

--- src/net/johnsonlau/android/bluetooth3/MainActivity.java
+++ src/net/johnsonlau/android/bluetooth3/MainActivity.java
@@ -1,15 +1,34 @@
 package net.johnsonlau.android.bluetooth3;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
+    BluetoothAdapter mBluetoothAdapter;
+
+    public static int REQUEST_ENABLE_BT;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        init();
     }
 
+    private void init() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "no Bluetooth radio", Toast.LENGTH_LONG);
+        }
+
+        /*
+         * if (!mBluetoothAdapter.isEnabled()) { Intent enableBTIntent = new
+         * Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE);
+         * startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT); }
+         */
+    }
 }
