87cab8d4b3e3d87bd62083ed0d6142a02ba33016
Johnson Liu <gatement@gmail.com>
2013-10-27 08:06:37 +0800

    init project

---
+++ src/net/johnsonlau/android/bluetooth3/MainActivity.java
@@ -0,0 +1,15 @@
+package net.johnsonlau.android.bluetooth3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
