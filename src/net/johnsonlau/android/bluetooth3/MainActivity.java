00e3d296d0e56a06c3ff0947fa00153328c5b615
Johnson Liu <gatement@gmail.com>
2013-10-28 00:03:28 +0800

    list bonded devices

--- src/net/johnsonlau/android/bluetooth3/MainActivity.java
+++ src/net/johnsonlau/android/bluetooth3/MainActivity.java
@@ -1,12 +1,20 @@
 package net.johnsonlau.android.bluetooth3;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     BluetoothAdapter mBluetoothAdapter;
 
@@ -16,7 +24,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
         init();
+        showBondedDevices();
     }
 
     private void init() {
@@ -25,10 +35,27 @@ public class MainActivity extends Activity {
             Toast.makeText(this, "no Bluetooth radio", Toast.LENGTH_LONG);
         }
 
-        /*
-         * if (!mBluetoothAdapter.isEnabled()) { Intent enableBTIntent = new
-         * Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE);
-         * startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT); }
-         */
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBTIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    private void showBondedDevices() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+                .getBondedDevices();
+
+        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        for (BluetoothDevice device : pairedDevices) {
+            Map<String, String> dev = new HashMap<String, String>();
+            dev.put("name", device.getName());
+            dev.put("mac", device.getAddress());
+            list.add(dev);
+        }
+
+        setListAdapter(new SimpleAdapter(this, list, R.layout.device_item,
+                new String[] { "name", "mac" },
+                new int[] { R.id.name, R.id.mac }));
     }
 }
